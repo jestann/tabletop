36a1b8f30ec121828edfaf1a8ca316174018ef8c
Jess Bird <jessannbird@gmail.com>
2017-06-18 19:47:08 +0000

    Retrieve authentication token - testing

--- renderer.js
+++ renderer.js
@@ -1,3 +1,55 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+// Retrieve Auth Token with initialize
+
+function initialize (email, password) {
+  const baseUrl = 'https://www.bloc.io/api/v1';
+  fetch(`${baseUrl}/sessions`, {
+    method: 'POST', 
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify({email: email, password: password})}
+  ).then(
+    response => { response.json().then( data => { 
+      const token = data.auth_token;
+      if (token) { return token; }
+      })
+    }
+  ).catch( error => { console.log(error) })
+}
+
+
+// VIEW functionality for initialize
+
+function initialize (email, password) {
+  const baseUrl = 'https://www.bloc.io/api/v1';
+  fetch(`${baseUrl}/sessions`, {
+    method: 'POST', 
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify({email: email, password: password})}
+  ).then( 
+    response => { response.json().then( data => { 
+      const token = data.auth_token;
+      if (token) { return token; }
+      })
+    }
+  ).catch( error => { console.log(error) })
+}
+
+let email = "";
+let password = "";
+let authToken = "";
+let emailBox = document.getElementById("email");
+let passwordBox = document.getElementById("password");
+let submit = document.getElementById("submit");
+let returned = document.getElementById('returned');
+
+submit.addEventListener('click', function() {
+  email = emailBox.value;
+  password = passwordBox.value;
+  authToken = initialize (email, password);
+  // authToken = "babjasdjasklfjk";
+  returned.innerHTML = `${authToken}`;
+});
