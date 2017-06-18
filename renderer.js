d15a03883105713f504fec41736d82a4379340ff
Jess Bird <jessannbird@gmail.com>
2017-06-18 20:53:06 +0000

    Testing retrieval of user information

--- renderer.js
+++ renderer.js
@@ -20,24 +20,59 @@ function initialize (email, password) {
   ).catch( error => { console.log(error) } );
 }
 
-// VIEW functionality for initialize
+
+// Retrieve users with getMe
+
+function getMe (authToken) {
+  fetch(`${baseUrl}/users/me`, {
+    method: 'Get',
+    headers: {'Content-Type': 'application/json', 'authorization': authToken},
+    }
+  ).then(
+      response => { JSON.parse(response) }
+  ).catch( error => { console.log(error) })
+}
+
+
+// VIEW functionality for initialize and getMe
 
 let email = "";
 let password = "";
 let authToken = "";
-let emailBox = document.getElementById("email");
-let passwordBox = document.getElementById("password");
+let userObject = {};
+
+let emailBox = document.getElementById("input-email");
+let passwordBox = document.getElementById("input-password");
 let submit = document.getElementById("submit");
 let returned = document.getElementById('returned');
+
+let user = document.getElementById("user");
+let userId = document.getElementById("user-id");
+let userEmail = document.getElementById("user-email");
+let userCreatedAt = document.getElementById("user-created-at");
 
 submit.addEventListener('click', function() {
   email = emailBox.value;
   password = passwordBox.value;
   authToken = initialize (email, password);
   // authToken = "babjasdjasklfjk";
+  
   if (authToken !== undefined) {
     returned.innerHTML = `${authToken}`;
   } else {
     returned.innerHTML = "error retrieving token";
+  }
+  
+  if (authToken) {
+    userObject = getMe(authToken);
+    // userObject = { id: "0000", email: "test@test.com", created_at: "today" };
+    
+    userId.innerHTML = `User ID: ${userObject.id}`;
+    userEmail.innerHTML = `User Email: ${userObject.email}`;
+    userCreatedAt.innerHTML = `User Created At: ${userObject.created_at}`;
+  } else {
+    userId.innerHTML = "No user information available.";
+    userEmail.innerHTML = "";
+    userCreatedAt.innerHTML = "";
   }
 });
