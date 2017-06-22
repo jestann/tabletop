3558fce547feee4aa51df5055032536665da7fb1
Jess Bird <jessannbird@gmail.com>
2017-06-22 05:14:54 +0000

    Refactoring code to test for styling and view

--- renderer.js
+++ renderer.js
@@ -1,230 +1,216 @@
+// NOTE from ELECTRON DOCS.
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-// initial data
+// Initialize Kele Client
 
-const baseUrl = 'https://www.bloc.io/api/v1';
+// const Kele = require('./Kele')
+// const kele = new Kele()
+// kele.initalize()
 
 
-// Retrieve auth token with initialize
 
-async function initialize (email, password) {
-  try {
-    let response = await fetch(`${baseUrl}/sessions`, {
-      method: 'POST', 
-      headers: {'Content-Type': 'application/json'}, 
-      body: JSON.stringify({email: email, password: password})
-    });
-    let data = await response.json();
-    if (data.auth_token) { return data.auth_token; }
+class Kele {
+  constructor() {
+    this.baseUrl = 'https://www.bloc.io/api/v1'
+    this.authToken = null
+    this.userObject = null
+    this.mentorSchedule = null
   }
-  catch(error) {
-    console.log(error);
+  
+  async initialize (email, password) {
+    try {
+      let response = await fetch(`${this.baseUrl}/sessions`, {
+        method: 'POST', 
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({email: email, password: password})
+      })
+      let data = await response.json()
+      this.authToken = data.auth_token
+      return this.authToken
+    } 
+    catch(error) { console.log(error) }
   }
-}
-// could this be a better way to write the initialize function?
 
-
-
-// Retrieve users with getMe
-
-function getMe (authToken) {
-  fetch(`${baseUrl}/users/me`, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json', 'authorization': authToken},
+  async getMe () {
+    try {
+      let response = await fetch(`${this.baseUrl}/users/me`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'authorization': this.authToken},
+      })
+      this.userObject = await JSON.parse(response)
+      return this.userObject
     }
-  ).then(
-      response => { JSON.parse(response) }
-  ).catch( error => { console.log(error) })
-}
-
-
-
-// VIEW functionality for initialize and getMe
-
-let email = "";
-let password = "";
-let authToken = "";
-let userObject = {};
-
-let emailBox = document.getElementById("input-email");
-let passwordBox = document.getElementById("input-password");
-let loginSubmit = document.getElementById("login-submit");
-let returned = document.getElementById('returned');
-
-let user = document.getElementById("user");
-let userId = document.getElementById("user-id");
-userId.innerHTML = "No user information available."
-let userEmail = document.getElementById("user-email");
-let userCreatedAt = document.getElementById("user-created-at");
-
-loginSubmit.addEventListener('click', function() {
-  email = emailBox.value;
-  password = passwordBox.value;
-  authToken = initialize (email, password);
-  
-  // FOR TESTING:
-  // authToken = "babjasdjasklfjk";
-  
-  if (authToken !== undefined) {
-    returned.innerHTML = `${authToken}`;
-  } else {
-    returned.innerHTML = "error retrieving token";
+    catch(error) { console.log(error) }
   }
   
-  if (authToken) {
-    userObject = getMe(authToken);
-    
-    // FOR TESTING:
-    // userObject = { id: "0000", email: "test@test.com", created_at: "today", mentor_id: 0 };
-    
-    userId.innerHTML = `User ID: ${userObject.id}`;
-    userEmail.innerHTML = `User Email: ${userObject.email}`;
-    userCreatedAt.innerHTML = `User Created At: ${userObject.created_at}`;
-  } else {
-    userId.innerHTML = "No user information available.";
-    userEmail.innerHTML = "";
-    userCreatedAt.innerHTML = "";
+  async getMentorAvailability (mentorId) {
+    try {
+      let response = await fetch(`${this.baseUrl}/mentors/${mentorId}/student_availability`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'authorization': this.authToken},
+        body: `{ "id": ${mentorId} }`
+      })
+      this.mentorSchedule = await response.map((obj) => JSON.parse(obj))
+      return this.mentorSchedule
+    }
+    catch(error) { console.log(error) }
   }
-});
-
-
-
-// Retrieve mentor availability
-
-function getMentorAvailability (mentorId) {
-  if (authToken) {
-    fetch(`${baseUrl}/mentors/${mentorId}/student_availability`, {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json', 'authorization': authToken},
-      body: `{ "id": ${mentorId} }`
+  
+  async getRoadmap (roadmapId) {
+    try {
+      let response = await fetch(`${this.baseUrl}/roadmaps/${roadmapId}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'authorization': this.authToken},
+        body: `{ "id": ${roadmapId} }`
       }
-    ).then(
-        response => { response.map((obj) => JSON.parse(obj)) }
-    ).catch( error => { console.log(error) })
-  } else {
-    return "authentication error";
+      this.roadmap = await response.map((obj) => JSON.parse(obj))
+      return this.roadmap
+    }
+    catch(error) { console.log(error) }
+  }
+  
+  async getCheckpoint (checkpointId) {
+    try {
+      let response = await fetch(`${this.baseUrl}/checkpoints/${checkpointId}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'authorization': this.authToken},
+        body: `{ "id": ${checkpointId} }`
+      }
+      this.checkpoint = await response.map((obj) => JSON.parse(obj))
+      return this.checkpoint
+    }
+    catch(error) { console.log(error) }
   }
 }
 
+// module.exports = Kele
 
-// Some view functionality for mentor availability
-
-let scheduleBox = document.getElementById("schedule");
-scheduleBox.innerHTML = "No user information available."
-let wantBox = document.getElementById("want-box");
-let mentorSubmit = document.getElementById("mentor-submit");
-
-function updateMentorSchedule () {
+class KeleView {
+  constructor() {
+    // const Kele = require('Kele')
+    this.kele = new Kele()
+  }
   
-  // FOR TESTING:
-  // let authToken = "jjj";
-  // let userObject = {};
-  
-  if (authToken && userObject) {
-    let schedule = getMentorAvailability (userObject.mentor_id);
-    scheduleBox.innerHTML = "";
-    wantBox.innerHTML = "";
+  async initializeView {
+    let email = ''
+    let password = ''
 
-    // FOR TESTING:
-    /*
-    let schedule = [
-      { starts_at: 1, ends_at: 2 },
-      { starts_at: 2, ends_at: 3 },
-      { starts_at: 3, ends_at: 4 }
-    ];
-    */
+    const loginBox = document.getElementsByClassName('login')[0]
+    const emailBox = document.getElementById('input-email')
+    const passwordBox = document.getElementById('input-password')
+    const loginSubmit = document.getElementById('login-submit')
+
+    const userBox = document.getElementsByClassName('user')[0]
+    const userInfo = document.getElementById('user-info')
     
-    schedule.forEach((availObject) => {
-      let timeBox = document.createElement('div');
-      timeBox.className = "timebox t-hover";
-      let start = document.createElement('p');
-      start.innerHTML = `Starts At: ${availObject.starts_at}`;
-      let end = document.createElement('p');
-      end.innerHTML = `Ends At: ${availObject.ends_at}`;
-      let claim = document.createElement('div');
-      claim.className = "timebox timebox-button";
-      claim.innerHTML = "Claim this Spot!";
-      timeBox.appendChild(start);
-      timeBox.appendChild(end);
-      timeBox.appendChild(claim);
+    const roadmapBox = document.getElementsByClassName('roadmap')[0]
+    const checkpointBox = document.getElementsByClassName('checkpoint')[0]
+    
+
+    async loginListener () {
+      email = emailBox.value
+      password = passwordBox.value
       
-      claim.addEventListener("click", function(event) {
-        scheduleBox.childNodes.forEach((timeBox) => { 
-          timeBox.childNodes[2].className = 'timebox timebox-button';
-          timeBox.childNodes[2].innerHTML = "Claim this Spot!";
-        });
-        event.target.className = "timebox selected";
-        event.target.innerHTML = "This spot claimed.";
+      // how to best handle this?
+      kele.authToken = await kele.initialize (email, password)
+      console.log(`auth token retrieved: ${kele.authToken}`)
+      
+      kele.userObject = await kele.getMe(kele.authToken)
+      console.log(kele.userObject)
 
-        let savedBox = document.createElement('div');
-        savedBox.className = "timebox want-box";
-        let claimedIt = document.createElement('div');
-        claimedIt.innerHTML = "This Time Reserved:";
-        claimedIt.className = "want-box-claim";
-        let startTime = document.createElement('p');
-        startTime.innerHTML = `Starts At: ${availObject.starts_at}`;
-        let endTime = document.createElement('p');
-        endTime.innerHTML = `Ends At: ${availObject.ends_at}`;
-        savedBox.appendChild(claimedIt);
-        savedBox.appendChild(startTime);
-        savedBox.appendChild(endTime);
-
-        wantBox.innerHTML = "";
-        wantBox.appendChild(savedBox);
-      });
-
-      scheduleBox.appendChild(timeBox);
-    });
-  } else {
-    scheduleBox.innerHTML = "No user information available."
-  }
-}
-
-updateMentorSchedule();
-mentorSubmit.addEventListener('click', updateMentorSchedule);
-
-
-
-// move this to a module?
-// Retrieve roadmaps
-
-function getRoadmap (roadmapId) {
-  if (authToken) {
-    fetch(`${baseUrl}/roadmaps/${roadmapId}`, {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json', 'authorization': authToken},
-      body: `{ "id": ${roadmapId} }`
+      if (kele.userObject) {
+        loginBox.className = 'login hidden'
+        
+        userBox.className = 'user'
+        userInfo.innerHTML = `Welcome to Bloc ${kele.userObject.name}!`
+        
+        mentorBox.className = 'mentor'
+        roadmapBox.className = 'roadmap'
+        checkpointBox.className = 'checkpoint'
+        
+      } else {
+        userBox.className = 'user error'
+        userInfo.innerHTML = 'No user information available. Please try logging in again.'
       }
-    ).then(
-        response => { response.map((obj) => JSON.parse(obj)) }
-    ).catch( error => { console.log(error) })
-  } else {
-    return "authentication error";
+    }
+        
+    loginSubmit.addEventListener('click', loginListener)
   }
-}
-// I'm not sure exactly what kind of data this returns, 
-// as the API docs suggest it's thes same return as mentor availability
-
-
-
-// Retrieve checkpoints
-
-function getCheckpoint (checkpointId) {
-  if (authToken) {
-    fetch(`${baseUrl}/checkpoints/${checkpointId}`, {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json', 'authorization': authToken},
-      body: `{ "id": ${checkpointId} }`
+  
+  async setupMentorView () {
+  
+    const mentorBox = document.getElementsByClassName('mentor')[0]
+    const mentorSubmit = document.getElementById("mentor-submit");
+    
+    async refreshMentorView {
+      const scheduleBox = document.getElementById("schedule-id");
+      const wantBox = document.getElementById("want-box-id");
+      
+      const schedule = await kele.getMentorAvailability(kele.userObject.mentor_id)
+      scheduleBox.innerHTML = ''
+      wantBox.innerHTML = ''
+      
+      schedule.forEach((availObject) => {
+        let timeBox = document.createElement('div')
+        timeBox.className = 'timebox t-hover tabling'
+        let start = document.createElement('p')
+        start.innerHTML = `Starts At: ${availObject.starts_at}`
+        let end = document.createElement('p')
+        end.innerHTML = `Ends At: ${availObject.ends_at}`;
+        let claim = document.createElement('div');
+        claim.className = "timebox timebox-button";
+        claim.innerHTML = "Claim this Spot!";
+        timeBox.appendChild(start);
+        timeBox.appendChild(end);
+        timeBox.appendChild(claim);
+      
+        claim.addEventListener("click", function(event) {
+          scheduleBox.childNodes.forEach((timeBox) => { 
+            timeBox.childNodes[2].className = 'timebox timebox-button';
+            timeBox.childNodes[2].innerHTML = "Claim this Spot!";
+          })
+          event.target.className = "timebox selected"
+          event.target.innerHTML = "This spot claimed."
+  
+          let savedBox = document.createElement('div')
+          savedBox.className = "timebox want-box tabling"
+          let claimedIt = document.createElement('div')
+          claimedIt.innerHTML = "This Time Reserved:"
+          claimedIt.className = "want-box-claim"
+          let startTime = document.createElement('p')
+          startTime.innerHTML = `Starts At: ${availObject.starts_at}`
+          let endTime = document.createElement('p');
+          endTime.innerHTML = `Ends At: ${availObject.ends_at}`;
+          savedBox.appendChild(claimedIt);
+          savedBox.appendChild(startTime);
+          savedBox.appendChild(endTime);
+  
+          wantBox.innerHTML = "";
+          wantBox.appendChild(savedBox);
+        })
+  
+        scheduleBox.appendChild(timeBox);
       }
-    ).then(
-        response => { response.map((obj) => JSON.parse(obj)) }
-    ).catch( error => { console.log(error) })
-  } else {
-    return "authentication error";
+    }
+    
+    refreshMentorView()
+    mentorSubmit.addEventListener('click', refreshMentorView)
+  }
+  
+  async initialize {
+    await initializeView()
+    await refreshMentorView()
+    // refreshRoadmapView()
+    // refreshCheckpointView()
   }
 }
+
+const keleView = new KeleView()
+keleView.initalize()
+
+
 
 
 /*
